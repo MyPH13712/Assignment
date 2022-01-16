a5dc63caac836df194460b9532c8e8b568b5124d
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-01-16 11:10:08 +0700

    Update footer.js

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -3,6 +3,32 @@ const Footer = {
         return /* html */`<div class="2xl:container 2xl:mx-auto bg-zinc-100 mt-5 py-4 text-center text-white">
              
         <div class="mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 px-4 py-12">
+        
+        <div class="2xl:container 2xl:mx-auto md:py-12 py-3">
+            <div class=" grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-8 md:gap-12 gap-14 lg:px-20 lg:py-5 md:px-12 px-4">
+                <!-- Delivery grid Card -->
+                <div class="">
+                    <img class="" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/highlight-1-svg1.svg" alt="delivery">
+                    <h3 class="text-xl leading-5 font-semibold text-black lg:mt-10 mt-8">Giao hàng</h3>
+                    <p class="text-base leading-6 font-normal text-black mt-4 lg:w-full md:w-9/12 w-full">Giao hàng miễn phí cho các đơn đặt hàng trên 50.000 đ</p>
+                </div>
+                <!-- customer Grid Card -->
+                <div class="">
+                    <img class="" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/highlight-1-svg2.svg" alt="Customer Care">
+                    <h3 class="text-xl leading-5 font-semibold text-black lg:mt-10 mt-8">Chăm sóc khách hàng</h3>
+                    <p class="text-base leading-6 font-normal text-black mt-4 lg:w-full md:w-9/12 w-full">Dịch vụ chăm sóc khách hàng luôn 24/7 tại  <span class="font-semibold cursor-pointer">+495-589-509</span></p>
+                </div>
+                <!-- MonyBack Guarantee card -->
+                <div class="">
+                    <img class="" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/highlight-4-svg3.svg" alt="Money Back Guarantee">
+                    <div>
+                        <p class="text-xl leading-5 font-semibold text-black lg:mt-10 mt-8">Hoàn lại tiền</p>
+                        <p class="text-base leading-6 font-normal text-black mt-4 lg:w-full md:w-9/12 w-full">Bạn có thể trả lại sản phẩm trong vòng 14 ngày</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    
         <div class="flex flex-col items-center justify-center">
             <div>
             <svg class="  hover:text-teal-500" width="111" height="19" viewBox="0 0 111 19" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
