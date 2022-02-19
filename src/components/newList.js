e5681a33f13cc2834464a6a3a34570794ede0487
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-19 17:29:17 +0700

    update db

--- src/components/newList.js
+++ src/components/newList.js
@@ -11,7 +11,7 @@ const NewList = {
             <div class="grid md:grid-cols-2 lg:grid-cols-4 justify-items-between mt-8 gap-y-8 lg:gap-y-0 gap-x-8">
                 ${response.data.map((product) => `
                 <div class="relative">
-                <div class="relative group">
+                <div class="relative group py-5">
                     <div class="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
                         <a href="/products/${product.id}">
                             <img class="w-full"  src="${product.img}" alt="" />
@@ -20,6 +20,7 @@ const NewList = {
                     <p class="font-normal dark:text-black text-xl leading-5 text-stone-900 md:mt-6 mt-4"><a href="/products/${product.id}">${product.name}</a></p>
                     <p class="font-semibold dark:text-black text-xl leading-5 text-stone-900 mt-4">${product.price}</p>
                 </div>
+                
                 `).join("")}
             </div>
         </div>
@@ -31,7 +32,7 @@ const NewList = {
             <div class="grid md:grid-cols-2 lg:grid-cols-4 justify-items-between mt-8 gap-y-8 lg:gap-y-0 gap-x-8">
             ${response.data.map((product) => `
                 <div class="relative">
-                <div class="relative group">
+                <div class="relative group py-5">
                     <div class="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
                         <a href="/products/${product.id}">
                             <img class="w-full"  src="${product.img}" alt="" />
