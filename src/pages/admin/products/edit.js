58eb89101c144fb7f19ff250b15169c4873539fe
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-22 17:55:36 +0700

    upadte

--- src/pages/admin/products/edit.js
+++ src/pages/admin/products/edit.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import NavAdmin from "../../../components/NavAdmin";
-import { update, get } from "../../../api/posts";
+import { update, get } from "../../../api/product";
 
 const AdminEditProduct = {
     async render(id) {
