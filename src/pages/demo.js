42328134ff56d3b8b058a4444ef9eb5da5f90396
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-22 19:18:43 +0700

    Update demo.js

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,10 +1,11 @@
 <div class="max-w-5xl mx-auto">
+                
                 <div class="news">
-                    <form id="formEditPost">
-                        <input type="text" class="border border-black" id="title-post" placeholder="Title Post" value="${data.title}"/><br />
-                        <img src="${data.img}" id="img-preview" />
+                    <form id="formAddPost">
+                        <input type="text" class="border border-black" id="title-post" name="title-post" placeholder="Title Post"/><br />
                         <input type="file" class="border border-black" id="img-post" /> <br />
-                        <textarea name="" class="border border-black" id="desc-post" cols="30" rows="10">${data.desc}</textarea> <br />
+                        <img width="200" src="https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg" id="img-preview"/>
+                        <textarea name="" class="border border-black" id="desc-post" cols="30" rows="10"></textarea> <br />
                         <button class="bg-blue-500 inline-block px-3 py-4">Add post</button>
                     </form>
                 </div>
